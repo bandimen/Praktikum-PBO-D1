999b30749c7d83680ddbda0f4754cf121b9d0dc1
bandimen <121479982+bandimen@users.noreply.github.com>
2024-02-21 11:40:10 +0700

    Add files via upload

---
+++ Pertemuan 1/Titik/src/MTitik.java
@@ -0,0 +1,22 @@
+public class MTitik {
+    public static void main(String[] args){
+        Titik t1;
+        Titik t2;
+        Titik t3;
+        
+        t1 = new Titik();
+        t1.setAbsis(1);
+        t1.setOrdinat(2);
+        
+        t2 = new Titik();
+        t2.setAbsis(3);
+        t2.setOrdinat(4);
+        
+        t3 = new Titik(5, 6);
+        
+        System.out.println("Jumlah objek titik : " + t2.getCounter());
+        System.out.println("T1 = (" + t1.getAbsis() + ", " + t1.getOrdinat() + ")");
+        System.out.println("T2 = (" + t2.getAbsis() + ", " + t2.getOrdinat() + ")");
+        System.out.println("T3 = (" + t3.getAbsis() + ", " + t3.getOrdinat() + ")");
+    }
+}
