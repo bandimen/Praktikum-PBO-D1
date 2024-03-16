27312cf6d2cd4a0d364bd0905c4670c44b2f727d
bandimen <121479982+bandimen@users.noreply.github.com>
2024-03-16 22:36:20 +0700

    Update Person.java

--- Pertemuan 4/Tugas Tambahan/src/org/person/Person.java
+++ Pertemuan 4/Tugas Tambahan/src/org/person/Person.java
@@ -32,4 +32,9 @@ public class Person {
     public void setAddress(String address){
         this.address = address;
     }
+    public void getDetails(){
+        System.out.println("Nama    : " + name);
+        System.out.println("Umur    : " + age);
+        System.out.println("Alamat  : " + address);
+    }
 }
